db5726c33db79124544925846cc12b378679433f
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2022-02-10 00:49:30 -0500

    Added methods for R&B values on the color sensor

--- src/main/java/frc/robot/subsystems/TempTower.java
+++ src/main/java/frc/robot/subsystems/TempTower.java
@@ -5,22 +5,18 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.ColorSensorV3;
-import com.revrobotics.Rev2mDistanceSensor;
-import com.revrobotics.Rev2mDistanceSensor.Unit;
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
 
 public class TempTower extends SubsystemBase {
 
   private final I2C.Port port = I2C.Port.kOnboard;
-
   private final ColorSensorV3 colorSensor = new ColorSensorV3(port);
 
-  private ShuffleboardTab debug = Shuffleboard.getTab("debug");
+  private DoubleSupplier red = () -> 0.0;
+  private DoubleSupplier blue = () -> 0.0;
 
   /** Creates a new TempBase. */
   public TempTower() {}
@@ -29,16 +25,34 @@ public class TempTower extends SubsystemBase {
     return this.colorSensor;
   }
 
+  public double getRed() {
+    return this.red.getAsDouble();
+  }
+
+  public double getBlue() {
+    return this.blue.getAsDouble();
+  }
+
+  public DoubleSupplier getRedSupplier() {
+    return this.red;
+  }
+
+  public DoubleSupplier getBlueSupplier() {
+    return this.blue;
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    // double dist = this.sensor.getRange(Unit.kInches);
 
     if (colorSensor != null) {
+      this.blue = () -> colorSensor.getBlue();
+      this.red = () -> colorSensor.getRed();
+
       SmartDashboard.putNumber("blue", colorSensor.getRawColor().blue);
       SmartDashboard.putNumber("red", colorSensor.getRawColor().red);
-      SmartDashboard.putNumber("green", colorSensor.getRawColor().green);
+      SmartDashboard.putNumber("ir", colorSensor.getIR());
+      SmartDashboard.putNumber("proximity", colorSensor.getProximity());
     }
-    // debug.add("distance", dist);
   }
 }
