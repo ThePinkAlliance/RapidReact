8a6f53c917009e57e1bf6e6fce174ee58476b926
devsamuelv <realalloutmusic@gmail.com>
2022-09-23 01:28:29 -0400

    Started work on the interpolation function

--- src/main/java/frc/robot/LinearInterpolationTable.java
+++ src/main/java/frc/robot/LinearInterpolationTable.java
@@ -4,7 +4,24 @@
 
 package frc.robot;
 
-/** Add your docs here. */
-public class LinearInterpolationTable {
+import java.util.List;
 
+import edu.wpi.first.wpilibj.drive.Vector2d;
+
+/** Add your docs here */
+public class LinearInterpolationTable {
+  /*
+   * To get a better understanding of how linear interpolation works.
+   * https://en.wikipedia.org/wiki/Linear_interpolation
+   */
+  List<Vector2d> points;
+
+  public LinearInterpolationTable(List<Vector2d> points) {
+    this.points = points;
+  }
+
+  private double interp(double e, Vector2d a, Vector2d b) {
+    // https://matthew-brett.github.io/teaching/linear_interpolation.html
+    return a.y + (e - a.x) * ((b.y - a.y) / (b.x - a.x));
+  }
 }
