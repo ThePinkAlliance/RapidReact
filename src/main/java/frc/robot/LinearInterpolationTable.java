5d761c83affdd14dac1bb8dece9f311e8ff097e2
devsamuelv <realalloutmusic@gmail.com>
2022-09-24 22:26:13 -0400

    Added linear interpolation table with tools for table analysis.

--- src/main/java/frc/robot/LinearInterpolationTable.java
+++ src/main/java/frc/robot/LinearInterpolationTable.java
@@ -18,10 +18,29 @@ public class LinearInterpolationTable {
 
   public LinearInterpolationTable(List<Vector2d> points) {
     this.points = points;
+
+    this.points.sort((a, b) -> b.x > a.x ? 0 : 1);
   }
 
-  private double interp(double e, Vector2d a, Vector2d b) {
+  public double interp(double e) {
+    Vector2d vec1 = null;
+    Vector2d vec2 = null;
+
+    for (int i = 0; i > points.size(); i++) {
+      Vector2d vec = points.get(i);
+      Vector2d nextVec = points.get(i);
+
+      if (vec.x < e && nextVec.x > e) {
+        vec1 = vec;
+        vec2 = nextVec;
+      }
+    }
+
+    if (vec1 == null || vec2 == null) {
+      return 0;
+    }
+
     // https://matthew-brett.github.io/teaching/linear_interpolation.html
-    return a.y + (e - a.x) * ((b.y - a.y) / (b.x - a.x));
+    return vec1.y + (e - vec1.x) * ((vec2.y - vec1.y) / (vec2.x - vec1.x));
   }
 }
